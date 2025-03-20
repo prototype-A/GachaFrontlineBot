e8167742240b482fe84ceeebff12e10b8eb709cd
prototype-A <20850333+prototype-A@users.noreply.github.com>
2025-03-19 20:12:20 -0400

    Move error logging to implementing classes to display correct class names in output

--- app/src/main/java/com/prototypeA/discordbot/GachaFrontlineBot/handlers/IEventHandler.java
+++ app/src/main/java/com/prototypeA/discordbot/GachaFrontlineBot/handlers/IEventHandler.java
@@ -1,8 +1,5 @@
 package com.prototypeA.discordbot.GachaFrontlineBot.handlers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import discord4j.core.event.domain.Event;
 
 import reactor.core.publisher.Mono;
@@ -14,21 +11,20 @@ import reactor.core.publisher.Mono;
  */
 public interface IEventHandler<T extends Event> {
 
-    final Logger LOG = LoggerFactory.getLogger(IEventHandler.class);
-
-
     /**
      * Returns the class of the event type that the 
      * implementing handler will handle.
      * 
-     * @return The class of the event to handle
+     * @param <T> The type of the emitted Discord event that this 
+     * handler is tasked to handle.
+     * @return The class of the event to handle.
      */
     public Class<T> getEventClass();
 
     /**
-     * Returns the name of the class.
+     * Returns the name of this class.
      * 
-     * @return The name of this event handler
+     * @return The name of this event handler.
      */
     public default String getName() {
         return getClass().getSimpleName();
@@ -38,20 +34,19 @@ public interface IEventHandler<T extends Event> {
      * The method that is executed upon receiving an event of the 
      * type that the implementing handler is tasked to handle.
      * 
-     * @param event The event to handle
-     * @return An empty Mono upon completion of event handling
+     * @param <T> The type of the emitted Discord event that this 
+     * handler is tasked to handle.
+     * @param event The event to handle.
+     * @return An empty {@code Mono} upon completion of event handling.
      */
     public Mono<Void> handle(T event);
-    
+
     /**
      * The method that is executed when an error occurs 
      * during the handling of an event.
      * 
-     * @param error The error thrown
-     * @return An empty Mono after handling the error
+     * @param error The error thrown.
+     * @return An empty {@code Mono} after handling the error.
      */
-    public default Mono<Void> handleError(Throwable error) {
-        LOG.error("Failed to handle " + getEventClass().getSimpleName(), error);
-        return Mono.empty();
-    }
+    public Mono<Void> handleError(Throwable error);
 }
